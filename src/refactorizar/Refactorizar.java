29362b504a603d70fb88439110555dc2aec1ccd7
joterodelrio <joterodelrio@atanasoff16.danielcastelao.org>
2015-11-23 10:00:34 +0100

    Refactorizado

--- src/refactorizar/Refactorizar.java
+++ src/refactorizar/Refactorizar.java
@@ -1,61 +1,77 @@
-
 package refactorizar;
 
 /**
  * Lista los numeros primos de dos cifras
- * 
+ *
  */
 public class Refactorizar {
 
-    
     public static void main(String[] args) {
         boolean p = false;
-int numDigitos=0;
-        int ndigitos=0;
-        numDigitos=4;
-        if(numDigitos<=0)
-        System.out.println("Ingrese como parámetro, un numero de digitos correcto (mayor que 0): ");
-        for(int i = 1; i <= 99999; i++ )
-        {
-            int divisionEntera = i;
- 
-            int contador=0;
- 
-            while (divisionEntera != 0)
-        {
-            divisionEntera = divisionEntera / 10;
-            contador++;
-        }
-        ndigitos=contador;
-            
-            
-            if(ndigitos==numDigitos){
-                   if (i < 4) p = true;
-            else
-            {
-            if (i % 2 == 0) p = false; 
-                else
-                {
-                    int contador1 = 0;  
-                    int i1 = 1; 
-                    int limite = (i - 1) / 2; 
-                    if  (limite % 2 == 0) limite--;  
-             
-                    while(i1 <= limite)
-                    {
-                        if (i % i1 == 0) contador1++;
-                            i1 += 2;
-                            if (contador1 == 2) i1 = limite + 1;
-                    }
- 
-        if (contador1 == 1) p = true;
-            } 
-        } 
-     
-                if (p == true) 
-                    System.out.println(i);    
+        int numDigitos;
+        int ndigitos;
+        numDigitos = 4;
+        introduceDigitos(numDigitos);
+        for (int i = 1; i <= 99999; i++) {
+            int contador = cuentaNumDigitos(i);
+            ndigitos = contador;
+
+            p = calcularPrimo(ndigitos, numDigitos, i, p);
+            if (p == true) {
+                System.out.println(i);
             }
         }
     }
-    
+
+    private static boolean calcularPrimo(int ndigitos, int numDigitos, int i, boolean p) {
+        if (ndigitos == numDigitos) {
+            if (i < 4) {
+                p = true;
+            } else {
+                if (i % 2 == 0) {
+                    p = false;
+                } else {
+                    int contador1 = 0;
+                    int i1 = 1;
+                    int limite = (i - 1) / 2;
+                    if (limite % 2 == 0) {
+                        limite--;
+                    }
+                    
+                    while (i1 <= limite) {
+                        if (i % i1 == 0) {
+                            contador1++;
+                        }
+                        i1 += 2;
+                        if (contador1 == 2) {
+                            i1 = limite + 1;
+                        }
+                    }
+                    
+                    if (contador1 == 1) {
+                        p = true;
+                    }
+                }
+            }
+            
+        }
+        return p;
+    }
+
+    private static void introduceDigitos(int numDigitos) {
+        if (numDigitos <= 0) {
+            System.out.println("Ingrese como parámetro, un numero de digitos correcto (mayor que 0): ");
+        }
+    }
+
+    private static int cuentaNumDigitos(int i) {
+        int divisionEntera = i;
+        int contador = 0;
+        while (divisionEntera != 0) {
+            divisionEntera = divisionEntera / 10;
+            contador++;
+        }
+        return contador;
+    }
+
 }
